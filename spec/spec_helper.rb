f011f5e319f8bcaa147b05656066ad740ad0979f
dekokun <vagrant@cybird.(none)>
2013-06-12 08:34:25 +0200

    add PATH

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
 
 RSpec.configure do |c|
+  c.path = '/sbin:'
   if ENV['ASK_SUDO_PASSWORD']
     require 'highline/import'
     c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
